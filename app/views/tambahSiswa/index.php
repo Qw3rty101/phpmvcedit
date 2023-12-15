ae9ca56d8b8316ed0864415699ec5960aa4c67c5
Siraj Nurul Bil Haq <siraj.nurul@gmail.com>
2023-12-15 19:25:00 +0700

    fix ui

--- app/views/tambahSiswa/index.php
+++ app/views/tambahSiswa/index.php
@@ -49,43 +49,4 @@
         </div>
         <button type="submit" class="btn btn-primary">Tambah</button>
     </form>
-
-    <!-- DataTales Example -->
-    <div class="card shadow mb-4 mt-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Table Siswa</h6>
-        </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th>NISN</th>
-                            <th>Nama</th>
-                            <th>Rombel</th>
-                            <th>Kelas</th>
-                        </tr>
-                    </thead>
-                    <tfoot>
-                        <tr>
-                            <th>NISN</th>
-                            <th>Nama</th>
-                            <th>Rombel</th>
-                            <th>Kelas</th>
-                        </tr>
-                    </tfoot>
-                    <tbody>
-                        <?php foreach($data['siswa'] as $siswa) :?>
-                        <tr>
-                            <td><?= $siswa['nisn_siswa']; ?></td>
-                            <td><?= $siswa['name_siswa']; ?></td>
-                            <td><?= $siswa['rombel_siswa']; ?> </td>
-                            <td><?= $siswa['jurusan_siswa']; ?></td>
-                        </tr>
-                        <?php endforeach;?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
 </div>
